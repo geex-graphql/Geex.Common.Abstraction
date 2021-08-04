c2c56ce899bd3062ad5738a9a3b2ccd5304ec6d8
Lulus <snys98@outlook.com>
2021-08-04 17:45:16 +0800

    新增前缀查找的扩展方法

--- Extensions/System.Linq.cs
+++ Extensions/System.Linq.cs
@@ -10,33 +10,14 @@ namespace System.Linq
 {
     public static class LinqExtension
     {
-        ///// <summary>
-        ///// Filters a <see cref="IQueryable{T}"/> by given predicate if given condition is true.
-        ///// </summary>
-        ///// <param name="query">Queryable to apply filtering</param>
-        ///// <param name="condition">A boolean value</param>
-        ///// <param name="predicate">Predicate to filter the query</param>
-        ///// <returns>Filtered or not filtered query based on <paramref name="condition"/></returns>
-        //public static IQueryable<T> WhereIf<T>(this IQueryable<T> query, bool condition, Expression<Func<T, bool>> predicate)
-        //{
-        //    return condition
-        //        ? query.Where(predicate)
-        //        : query;
-        //}
+        public static string FindCommonPrefix(this string str, params string[] more)
+        {
+            var prefixLength = str
+                              .TakeWhile((c, i) => more.All(s => i < s.Length && s[i] == c))
+                              .Count();
 
-        ///// <summary>
-        ///// Filters a <see cref="IQueryable{T}"/> by given predicate if given condition is true.
-        ///// </summary>
-        ///// <param name="query">Queryable to apply filtering</param>
-        ///// <param name="condition">A boolean value</param>
-        ///// <param name="predicate">Predicate to filter the query</param>
-        ///// <returns>Filtered or not filtered query based on <paramref name="condition"/></returns>
-        //public static IQueryable<T> WhereIf<T>(this IQueryable<T> query, bool condition, Expression<Func<T, int, bool>> predicate)
-        //{
-        //    return condition
-        //        ? query.Where(predicate)
-        //        : query;
-        //}
+            return str[..prefixLength];
+        }
 
     }
 }
