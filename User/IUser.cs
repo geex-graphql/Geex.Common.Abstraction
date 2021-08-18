903b37337a0275df3824d9cfd85bfa30df9c7e7d
Lulus <snys98@outlook.com>
2021-08-18 19:38:21 +0800

    补充提交iuser

---
+++ User/IUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MongoDB.Entities;
+
+namespace Geex.Common.Abstraction.User
+{
+    public interface IUser : IEntity
+    {
+        string PhoneNumber { get; set; }
+        string UserName { get; set; }
+        string Email { get; set; }
+    }
+}
